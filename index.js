c17db7d9f054430823146351375c11858b637297
aletson <me@ajl.io>
2025-03-09 17:27:25 -0400

    te

--- index.js
+++ index.js
@@ -99,6 +99,7 @@ client.on('messageCreate', async message => {
                             }
                         }
                     } else {
+                        console.log(message.type);
                         if (attachments.length > 0) {
                             if (alter_info[0][0].pfp) {
                                 await webhook.send({ content: message.content.replace(alter_emote[0], ''), username: alter_info[0][0].name, avatarURL: alter_info[0][0].pfp, threadId: message.channel.id, files: attachments });
